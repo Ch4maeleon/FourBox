32e9652c9ce24c69be96d4e164c3750f9eaca9be
limhyunho <limhyunho@gmdsoft.com>
2023-12-20 19:46:39 +0900

    초기 커밋.

---
+++ Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FourBox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 3, 2, 2, 3 };
+            var s1 = new SortedSet<int>(arr);
+            var s2 = new HashSet<int>(arr);
+
+            Console.WriteLine("arr 출력 테스트");
+
+            foreach (int i in arr)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("set 출력 테스트");
+
+            foreach (int i in s1)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            s1.Add(4);
+            s1.Add(5);
+
+            s1.Remove(1);
+
+            Console.WriteLine();
+
+            Console.WriteLine("set 출력 테스트");
+
+            foreach (int i in s1)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(s1.Contains(4));
+
+            Console.WriteLine(s1.Count);
+
+            Console.WriteLine();
+
+            foreach (int i in s2)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            // sorted set : 순서가 있는 집합
+            // hash set : 순서가 없는 집합
+
+            // 데이터를 저장할때 바로 저장하지 않고 hast table을 사용하여 저장
+
+            // 중복된 값을 골라내야할 때
+            // 특정값이 있는지 빠르게 확인해야할 때
+            // 순서가 상관없을 때
+
+            // 만약 순서도 상관있으면서 중복제거 -> sorted set 사용
+
+            // 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FourBox
+{
+    public class FourBox
+    {
+        public HashSet<(int, int)> coordinates;
+
+        public FourBox(string line)
+        {
+            DrawRect(line);
+        }
+
+        public void DrawRect(string line)
+        {
+            coordinates = new HashSet<(int, int)>();
+
+            string[] values = line.Replace("  ", " ").Split();
+
+            for (int i = 0; i < values.Length; i += 4)
+            {
+                var x1 = int.Parse(values[i]);
+                var y1 = int.Parse(values[i + 1]);
+                var x2 = int.Parse(values[i + 2]);
+                var y2 = int.Parse(values[i + 3]);
+
+                for (int x = x1; x < x2; x++)
+                {
+                    for (int y = y1; y < y2; y++)
+                    {
+                        coordinates.Add((x, y));
+                    }
+                }
+            }
+
+            /*
+            foreach (var i in coordinates)
+            {
+                Console.WriteLine(i);
+            }
+            */
+        }
+
+        public int Area()
+        {
+            return coordinates.Count;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ln1 = "0 0 200 200  0 0 200 200  0 0 200 200  0 0 200 200";
+            string ln2 = "1 2 4 4  2 3 5 7  3 1 6 5  7 3 8 6";
+
+            FourBox boxes1 = new FourBox(ln1);
+            FourBox boxes2 = new FourBox(ln2);
+
+            Console.WriteLine($"Boxes1 Area : {boxes1.Area()}");
+            Console.WriteLine($"Boxes2 Area : {boxes2.Area()}");
+        }
+    }
+}
